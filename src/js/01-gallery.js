b5a19cf0fbc875aa50bc53a98f3db82414786a99
Vadym8177 <vadim.8177@icloud.com>
2022-08-06 22:16:41 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const galleryItemsRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const listItems = createListItems(galleryItems);
+function createListItems(items) {
+  return items
+    .map(
+      item =>
+        `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    )
+    .join('');
+}
+galleryItemsRef.innerHTML = listItems;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
