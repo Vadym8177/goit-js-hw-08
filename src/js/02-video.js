c60274f5f440304e25f80383f7aa8daa35ddeb2c
Vadym8177 <vadim.8177@icloud.com>
2022-08-07 20:58:24 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const onPlay = function (data) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
+};
+
+player.on('timeupdate', onPlay);
+
+const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+const currentTimeParsed = JSON.parse(currentTime);
+
+player
+  .setCurrentTime(currentTimeParsed.seconds)
+  .then(function (seconds) {
+    
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
 
