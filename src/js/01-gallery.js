3ef9ebd67ca56ed68599eba537ce7655f25ad8be
Vadym8177 <vadim.8177@icloud.com>
2022-08-07 18:59:15 +0300

    task-3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,4 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 const galleryItemsRef = document.querySelector('.gallery');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+updateMessage();
+function onFormInput() {
+  const formdata = { email: form.email.value, message: form.message.value };
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formdata));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const email = e.target.email.value;
+  const message = e.target.message.value;
+
+  if (email !== '' && message !== '') {
+    const FormData = { email, message };
+    e.target.reset();
+    console.log(FormData);
+  } else {
+    alert('Все поля должны быть заполнены');
+  }
+    
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+
+function updateMessage() {
+    const savedMessage = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const savedMessageParsed = JSON.parse(savedMessage);
+
+
+    if (savedMessage) {
+        form.email.value = savedMessageParsed.email;
+        form.message.value = savedMessageParsed.message;
+    }
+
+}
